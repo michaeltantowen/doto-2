36e53b0db2f2bfde84014483304a38f3f54915d8
michaeltantowen <tantowen2002@gmail.com>
2021-06-04 15:37:21 +0700

    delete scirpit.js from some html

--- js/responsive.js
+++ js/responsive.js
@@ -5,3 +5,19 @@ const nav = document.querySelector(".nav ul");
 menuToggle.addEventListener("click", function () {
 	nav.classList.toggle("show");
 });
+// End Navbar
+
+// Contact Us
+function sendmsg() {
+	var email = document.getElementById("form-msg").email.value;
+	var msg = document.getElementById("form-msg").message.value;
+	if (email && msg) {
+		let ask = confirm("Send message?");
+		if (ask) {
+			console.log(email + " : " + msg);
+			alert("Your message has been sent");
+			document.getElementById("form-msg").reset();
+		}
+	}
+}
+// End Contact Us
--- js/script.js
+++ js/script.js
@@ -20,18 +20,3 @@ function showSlides() {
 	setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 // End Slideshow
-
-// Contact Us
-function sendmsg() {
-	var email = document.getElementById("form-msg").email.value;
-	var msg = document.getElementById("form-msg").message.value;
-	if (email && msg) {
-		let ask = confirm("Send message?");
-		if (ask) {
-			console.log(email + " : " + msg);
-			alert("Your message has been sent");
-			document.getElementById("form-msg").reset();
-		}
-	}
-}
-// End Contact Us
